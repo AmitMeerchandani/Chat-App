85c5efea14bc063d75228498bf0463a9773aaeb4
infinite-GUY <amitmeerchandani18@gmail.com>
2023-01-03 10:48:19 +0530

    added custom/broadcasting events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+let socket = io();
+
+socket.on("connect", function () {
+  console.log("Connected to the server");
+
+  socket.emit('createMessage', {
+    from: "BWB",
+    text: "What's UPP?"
+  })
+});
+
+socket.on("disconnect", function () {
+  console.log("Disconnected from server");
+});
+
+socket.on('newMessage', function (message) {
+    console.log("newMessage", message);
+});
