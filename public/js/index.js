25b8a0b5c8d6dfd24489556c11b4dc2ea8330abe
infinite-GUY <amitmeerchandani18@gmail.com>
2023-01-06 00:23:51 +0530

    added user direct message

--- public/js/index.js
+++ public/js/index.js
@@ -18,3 +18,14 @@ socket.emit('createMessage', {
 }, function (message){
   console.log(message, 'Got it.');
 });
+
+document.querySelector('#submit-btn').addEventListener('click', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: "User",
+    text: document.querySelector('input[name="message"]').value
+  }, function() {
+
+  })
+})
